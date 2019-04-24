7221ef35fa4c7a70fa8679f97d6cfc8c20bc52df
sdimichele <sabrinadimichele1992@gmail.com>
2019-04-23 22:06:32 -0500

    revised

--- store_item.rb
+++ store_item.rb
@@ -26,6 +26,7 @@ class Products
   def print_info
     puts "The item is a #{ item }.The color is #{ color }. The price is #{ price }."
   end
+end
 
   item_1 = Products.new("chair", "brown", 350)
   item_2 = Products.new("couch", "red", 900)
@@ -33,11 +34,8 @@ class Products
 
   puts item_1.item
   puts item_3.print_info
-  puts item_2.price = 499
-  puts item_2.price
   puts item_2.print_info
 
 
 
 
-end
